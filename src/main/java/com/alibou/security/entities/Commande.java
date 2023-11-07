5984e71ec4d8f3c2cfb6961ef708a68c1496f07c
koire-dev <wawouam@gmail.com>
2023-11-07 07:48:41 +0100

    mise en place des crud

---
+++ src/main/java/com/alibou/security/entities/Commande.java
@@ -0,0 +1,2 @@
+package com.alibou.security.entities;public class Commande {
+}
--- src/main/java/com/alibou/security/repository/UserRepository.java
+++ src/main/java/com/alibou/security/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.alibou.security.user;
 
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.alibou.security.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   Optional<User> findByEmail(String email);
